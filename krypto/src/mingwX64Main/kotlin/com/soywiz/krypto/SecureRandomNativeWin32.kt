863a5941700464832b397a232c63727bfb5b224e
soywiz <soywiz@gmail.com>
2019-08-20 22:41:16 +0200

    Fix .uncheckedCast -> reinterpret

--- krypto/src/mingwX64Main/kotlin/com/soywiz/krypto/SecureRandomNativeWin32.kt
+++ krypto/src/mingwX64Main/kotlin/com/soywiz/krypto/SecureRandomNativeWin32.kt
@@ -7,7 +7,8 @@ private val bscryptLibrary by lazy { LoadLibraryA("Bcrypt.dll") }
 // public fun BCryptGenRandom(hAlgorithm: platform.windows.BCRYPT_ALG_HANDLE? /* = kotlinx.cinterop.CPointer<out kotlinx.cinterop.CPointed>? */, pbBuffer: platform.windows.PUCHAR? /* = kotlinx.cinterop.CPointer<platform.windows.UCHARVar /* = kotlinx.cinterop.UByteVarOf<platform.windows.UCHAR /* = kotlin.UByte */> */>? */, cbBuffer: platform.windows.ULONG /* = kotlin.UInt */, dwFlags: platform.windows.ULONG /* = kotlin.UInt */): platform.windows.NTSTATUS /* = kotlin.Int */ { /* compiled code */ }
 private val BCryptGenRandomDynamic by lazy {
     GetProcAddress(bscryptLibrary, "BCryptGenRandom")
-        .uncheckedCast<CPointer<CFunction<Function4<BCRYPT_ALG_HANDLE?, PUCHAR?, ULONG, ULONG, Int>>>?>()!!
+		?.reinterpret<CFunction<Function4<BCRYPT_ALG_HANDLE?, PUCHAR?, ULONG, ULONG, Int>>>()
+		?: error("Can't find BCryptGenRandom @ Bcrypt.dll")
 }
 
 actual fun fillRandomBytes(array: ByteArray) {
