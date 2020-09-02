3020f3b6b4f766aca6e3b67d76285a62ee89615f
soywiz <soywiz@gmail.com>
2020-09-02 22:56:38 +0200

    Fix problem with global reference on NodeJS

--- krypto/src/jsMain/kotlin/com/soywiz/krypto/SecureRandomJs.kt
+++ krypto/src/jsMain/kotlin/com/soywiz/krypto/SecureRandomJs.kt
@@ -5,12 +5,12 @@ import org.khronos.webgl.Uint8Array
 
 private val isNodeJs by lazy { js("(typeof process === 'object' && typeof require === 'function')").unsafeCast<Boolean>() }
 private external fun require(name: String): dynamic
-private val global: dynamic = js("(typeof global !== 'undefined') ? global : self")
+private val _global: dynamic = js("((typeof global !== 'undefined') ? global : self)")
 
 actual fun fillRandomBytes(array: ByteArray) {
     if (isNodeJs) {
         require("crypto").randomFillSync(Uint8Array(array.unsafeCast<Int8Array>().buffer))
     } else {
-        global.crypto.getRandomValues(array)
+        _global.crypto.getRandomValues(array)
     }
 }
